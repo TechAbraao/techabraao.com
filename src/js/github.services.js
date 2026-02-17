925269302754ec71c64947a84e3bfe2bdfe21de0
TechAbraao <abraao.cutielips@hotmail.com>
2026-02-16 23:19:23 -0300

    feat: initial portfolio structure

---
+++ src/js/github.services.js
@@ -0,0 +1,45 @@
+async function getRepos(username) {
+    try {
+       
+        const response = await $.ajax({
+            method: "GET", 
+            url: `https://api.github.com/users/${username}/repos`
+        });
+        return response;
+    } catch (error) {
+        console.error("Error in HTTP request:", error);
+    }
+}
+
+function cardRepos(title, description, language) {
+    return `
+        <div class="card-projects">
+                <h3>${title}</h3>
+                <p>
+                    ${description}
+                </p>
+                <aside>
+                    <p>
+                        <div class="language-circle"></div>${language}
+                    <p>
+                </aside>
+            </div>
+    `
+}
+
+$(document).ready(async function() {
+    const res = await getRepos("TechAbraao"); 
+    const cardsSection = $("#cardsProjects")
+    
+    res.forEach(repo => {
+        let nameRepo = repo.name;
+        let descriptionRepo = repo.description;
+        let languageRepo = repo.language
+
+
+        const cardRepoHtml = cardRepos(nameRepo, descriptionRepo, languageRepo)
+        cardsSection.append(cardRepoHtml)
+    })
+
+    console.log(res)
+});
